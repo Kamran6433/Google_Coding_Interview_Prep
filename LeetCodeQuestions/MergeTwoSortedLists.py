9af1483fa1c94a0e2487a4728a0c50c6285a08c0
kamran6433 <kamran_12@icloud.com>
2022-10-05 14:02:06 +0100

    Finished a Google Interview question.

---
+++ LeetCodeQuestions/MergeTwoSortedLists.py
@@ -0,0 +1,20 @@
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
